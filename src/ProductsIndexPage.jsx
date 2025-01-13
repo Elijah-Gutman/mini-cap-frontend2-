743b19bc4a0658b563c8d9c8ed42deae7d5cca53
Elijah Gutman <gutmane7@gmail.com>
2025-01-13 15:40:57 -0500

    added search feature

--- src/ProductsIndexPage.jsx
+++ src/ProductsIndexPage.jsx
@@ -1,15 +1,26 @@
 import { useLoaderData, useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router";
+
 import { ProductsIndex } from "./ProductsIndex";
+
 export function ProductsIndexPage() {
-  const products = useLoaderData();
+  let products = useLoaderData();
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filterName = searchParams.get("name");
+  if (filterName) {
+    products = products.filter((product) => product.name.toLowerCase().includes(filterName.toLowerCase()));
+  }
+
   const handleShow = (product) => {
     console.log("handleShow", product);
     navigate(`/products/${product.id}`);
   };
+
   return (
-    <main className="container">
+    <div>
       <ProductsIndex products={products} onShow={handleShow} />
-    </main>
+    </div>
   );
 }
